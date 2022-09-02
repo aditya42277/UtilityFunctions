afa3fc718ec06c78899ce52da557e3103626b993
Aditya Bhattacharya <addy.ranjan@gmail.com>
2022-09-02 13:30:06 -0400

    Get row count function added

--- src/com/excel/functions/Driver.java
+++ src/com/excel/functions/Driver.java
@@ -1,8 +1,23 @@
 package com.excel.functions;
 
+import java.io.IOException;
+
 public class Driver {
+    private static String path = ".\\DataFiles\\student-data.xlsx";
+    private static String sheetName = "student-data";
+    private static ExcelOperations ex = new ExcelOperations(path, sheetName);
 
     public static void main(String[] args) {
+        int rowCount = 0;
+        try{
+            rowCount = ex.getRowCount();
+        }catch (IOException ex){
+            System.out.println(ex.getMessage());
+            System.out.println("The row count cannot be evaluated");
+        }
+
+        System.out.println("The row count is : " + rowCount);
+
 
     }
 }
